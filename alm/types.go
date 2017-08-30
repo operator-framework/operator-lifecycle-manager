9025a91fee4e458dd2bb961b33e1eef618895df1
EvB <erica.vonbuelow@coreos.com>
2017-08-30 17:10:44 -0400

    temp(alm): test out basic stuff

--- alm/types.go
+++ alm/types.go
@@ -7,26 +7,29 @@ import (
 )
 
 type OperatorInstaller interface {
-	Install(ctx context, ns string, data interface{}) error
+	Install(ctx context.Context, ns string, data interface{}) error
 }
 
 type KubeManifest struct {
-	client   *rest.RESTClient
-	resource string // resource name for operators
+	config *rest.Config
+	//client *rest.RESTClient
+	//	resource string // resource name for operators
 }
 
 // direct passthrough to k8s api - note data
-func (k *KubeManifest) Install(ctx context, ns string, data interface{}) error {
-	return k.client.Post().Context(ctx).
-		Namespace(ns).
-		Resource(k.resource).
-		Body(data).
-		Do()
-}
+// func (k *KubeManifest) Install(ctx context, ns string, data interface{}) error {
+// 	return k.client.Post().Context(ctx).
+// 		Namespace(ns).
+// 		Resource(k.resource).
+// 		Body(data).
+// 		Do()
+// }
 
 type MockInstall struct {
+	Name string
 }
 
-func (m *MockInstall) Install(ctx context, ns string, data []byte) error {
-
+func (m *MockInstall) Install(ctx context.Context, ns string, data []byte) error {
+	fmt.Printf("INSTALL %s: ctx=%+v ns=%s data=%s\n", m.Name, ctx, ns, data)
+	return nil
 }
--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/coreos-inc/alm/alm"
 	"net/http"
+	"os"
 )
+
+func doesShitWork() {
+	fmt.Println("testing...")
+	installer := alm.MockInstall{"does this shit even work?"}
+	err := installer.Install(context.TODO(), "alm-system", []byte("wat"))
+	fmt.Printf("Done. err=%s\n", err)
+	os.Exit(0)
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	doesShitWork()
 }
 
 func main() {
