5e6ef957825db712037babb5690e751d49949695
EvB <erica.vonbuelow@coreos.com>
2017-08-30 15:41:10 -0400

    feat(k8s): add crd and client util pkg

---
+++ k8s/client.go
@@ -0,0 +1,43 @@
+package k8sutil
+
+import (
+	"net"
+	"os"
+
+	"k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+func MustNewKubeExtClient() apiextensionsclient.Interface {
+	cfg, err := InClusterConfig()
+	if err != nil {
+		panic(err)
+	}
+	return clientset.NewForConfigOrDie(cfg)
+}
+
+func MustNewKubeClient() kubernetes.Interface {
+	cfg, err := InClusterConfig()
+	if err != nil {
+		panic(err)
+	}
+	return kubernetes.NewForConfigOrDie(cfg)
+}
+
+// TODO remove env vars to main
+func InClusterConfig() (*rest.Config, error) {
+	// Work around https://github.com/kubernetes/kubernetes/issues/40973
+	// See https://github.com/coreos/etcd-operator/issues/731#issuecomment-283804819
+	if len(os.Getenv("KUBERNETES_SERVICE_HOST")) == 0 {
+		addrs, err := net.LookupHost("kubernetes.default.svc")
+		if err != nil {
+			panic(err)
+		}
+		os.Setenv("KUBERNETES_SERVICE_HOST", addrs[0])
+	}
+	if len(os.Getenv("KUBERNETES_SERVICE_PORT")) == 0 {
+		os.Setenv("KUBERNETES_SERVICE_PORT", "443")
+	}
+	return rest.InClusterConfig()
+}
