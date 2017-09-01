2f8ce6bc6d84e358c45ce52ff3a22b4534b52849
EvB <erica.vonbuelow@coreos.com>
2017-09-01 13:07:19 -0400

    chore(vendor): update go deps

--- vendor/github.com/emicklei/go-restful/examples/restful-user-resource.go
+++ vendor/github.com/emicklei/go-restful/examples/restful-user-resource.go
@@ -141,12 +141,12 @@ func main() {
 		WebServicesUrl: "http://localhost:8080",
 		ApiPath:        "/apidocs.json",
 
-		// Optionally, specifiy where the UI is located
+		// Optionally, specify where the UI is located
 		SwaggerPath:     "/apidocs/",
 		SwaggerFilePath: "/Users/emicklei/xProjects/swagger-ui/dist"}
 	swagger.RegisterSwaggerService(config, wsContainer)
 
-	log.Printf("start listening on localhost:8080")
+	log.Print("start listening on localhost:8080")
 	server := &http.Server{Addr: ":8080", Handler: wsContainer}
 	log.Fatal(server.ListenAndServe())
 }
--- vendor/github.com/emicklei/go-restful/examples/restful-user-service.go
+++ vendor/github.com/emicklei/go-restful/examples/restful-user-service.go
@@ -133,11 +133,11 @@ func main() {
 		WebServicesUrl: "http://localhost:8080",
 		ApiPath:        "/apidocs.json",
 
-		// Optionally, specifiy where the UI is located
+		// Optionally, specify where the UI is located
 		SwaggerPath:     "/apidocs/",
 		SwaggerFilePath: "/Users/emicklei/Projects/swagger-ui/dist"}
 	swagger.InstallSwaggerService(config)
 
-	log.Printf("start listening on localhost:8080")
+	log.Print("start listening on localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
