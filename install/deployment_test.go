8ea52c8b9c3c06e042609e7d3642cd4f22e4ab5d
Evan Cordell <cordell.evan@gmail.com>
2017-09-25 15:48:34 -0400

    Fix deployment test

--- install/deployment_test.go
+++ install/deployment_test.go
@@ -1,58 +1,58 @@
 package install
 
-//
-//import (
-//	"fmt"
-//	"testing"
-//
-//	"github.com/coreos-inc/operator-client/pkg/client"
-//	"github.com/golang/mock/gomock"
-//	"github.com/stretchr/testify/assert"
-//	v1beta1extensions "k8s.io/api/extensions/v1beta1"
-//	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-//	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-//)
-//
-//func TestKubeDeployment(t *testing.T) {
-//	testDeploymentName := "alm-test-deployment"
-//	testDeploymentNamespace := "alm-test"
-//	testDeploymentLabels := map[string]string{"app": "alm", "env": "test"}
-//
-//	mockOwner := metav1.ObjectMeta{
-//		Name:         "operatorversion-owner",
-//		Namespace:    testDeploymentNamespace,
-//		GenerateName: fmt.Sprintf("%s-", testDeploymentNamespace),
-//	}
-//
-//	unstructuredDep := &unstructured.Unstructured{}
-//	unstructuredDep.SetName(testDeploymentName)
-//	unstructuredDep.SetNamespace("not-the-same-namespace")
-//	unstructuredDep.SetLabels(testDeploymentLabels)
-//
-//	labels := map[string]string{
-//		"alm-owned":           "true",
-//		"alm-owner-name":      mockOwner.Name,
-//		"alm-owner-namespace": mockOwner.Namespace,
-//	}
-//	deployment := v1beta1extensions.Deployment{
-//		ObjectMeta: metav1.ObjectMeta{
-//			Namespace:    testDeploymentNamespace,
-//			GenerateName: fmt.Sprintf("%s-", mockOwner.Name),
-//			Labels:       labels,
-//		},
-//	}
-//
-//	ctrl := gomock.NewController(t)
-//	defer ctrl.Finish()
-//
-//	mockClient := client.NewMockInterface(ctrl)
-//	mockClient.EXPECT().
-//		ListDeploymentsWithLabels(testDeploymentNamespace, labels).
-//		Return(&v1beta1extensions.DeploymentList{}, nil)
-//	mockClient.EXPECT().
-//		CreateDeployment(&deployment).
-//		Return(&deployment, nil)
-//
-//	deployInstallStrategy := &StrategyDetailsDeployment{[]v1beta1extensions.DeploymentSpec{deployment.Spec}}
-//	assert.NoError(t, deployInstallStrategy.Install(mockClient, mockOwner))
-//}
+import (
+	"fmt"
+	"testing"
+
+	"github.com/coreos-inc/operator-client/pkg/client"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	v1beta1extensions "k8s.io/api/extensions/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/labels"
+)
+
+func TestKubeDeployment(t *testing.T) {
+	testDeploymentName := "alm-test-deployment"
+	testDeploymentNamespace := "alm-test"
+	testDeploymentLabels := map[string]string{"app": "alm", "env": "test"}
+
+	mockOwner := metav1.ObjectMeta{
+		Name:         "operatorversion-owner",
+		Namespace:    testDeploymentNamespace,
+		GenerateName: fmt.Sprintf("%s-", testDeploymentNamespace),
+	}
+
+	unstructuredDep := &unstructured.Unstructured{}
+	unstructuredDep.SetName(testDeploymentName)
+	unstructuredDep.SetNamespace("not-the-same-namespace")
+	unstructuredDep.SetLabels(testDeploymentLabels)
+
+	almLabels := labels.Set{
+		"alm-owned":           "true",
+		"alm-owner-name":      mockOwner.Name,
+		"alm-owner-namespace": mockOwner.Namespace,
+	}
+	deployment := v1beta1extensions.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace:    testDeploymentNamespace,
+			GenerateName: fmt.Sprintf("%s-", mockOwner.Name),
+			Labels:       almLabels,
+		},
+	}
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockClient := client.NewMockInterface(ctrl)
+	mockClient.EXPECT().
+		ListDeploymentsWithLabels(testDeploymentNamespace, almLabels).
+		Return(&v1beta1extensions.DeploymentList{}, nil)
+	mockClient.EXPECT().
+		CreateDeployment(&deployment).
+		Return(&deployment, nil)
+
+	deployInstallStrategy := &StrategyDetailsDeployment{[]v1beta1extensions.DeploymentSpec{deployment.Spec}}
+	assert.NoError(t, deployInstallStrategy.Install(mockClient, mockOwner))
+}
