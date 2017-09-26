c524c7694eb42b322575c9cd8ab665d9f40e0f3f
EvB <erica.vonbuelow@coreos.com>
2017-09-26 17:24:41 -0400

    feat(apis/subscription): add cdr structs

---
+++ apis/subscription/v1alpha1/types.go
@@ -0,0 +1,39 @@
+package v1alpha1
+
+import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+const (
+	SubscriptionCRDName       = "apptype-v1s.app.coreos.com"
+	SubscriptionCRDAPIVersion = "apiextensions.k8s.io/v1beta1" // API version w/ CRD support
+
+	ApprovalAutomatic  = "automatic"
+	ApprovalUpdateOnly = "update-only"
+	ApprovalManual     = "manual"
+)
+
+// SubscriptionSpec defines an Application that can be installed
+type SubscriptionSpec struct {
+	Source   string `json:"source"`
+	AppType  string `json:"apptype"`
+	Channel  string `json:"channel"`
+	Approval string `json:"approval"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// Custom Resource of type "SubscriptionSpec"
+type Subscription struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata"`
+
+	Spec   *SubscriptionSpec `json:"spec"`
+	Status metav1.Status     `json:"status"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type SubscriptionList struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata"`
+
+	Items []*SubscriptionSpec `json:"items"`
+}
