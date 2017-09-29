42973562d6e4060ffed2035f1cb68b3bc96d17d4
EvB <erica.vonbuelow@coreos.com>
2017-09-29 11:40:42 -0400

    refactor(subscriptions): change pkg name and remove unused mock

--- subscriptions/operator.go
+++ subscriptions/operator.go
@@ -1,13 +1,11 @@
-package catalog
+package subscriptions
 
 import (
-	"fmt"
+	"errors"
 
-	installplanv1alpha1 "github.com/coreos-inc/alm/apis/installplan/v1alpha1"
-	subscriptionv1alpha1 "github.com/coreos-inc/alm/apis/subscription/v1alpha1"
 	"github.com/coreos-inc/operator-client/pkg/client"
-	//	"github.com/coreos/go-semver/semver"
 
+	subscriptionv1alpha1 "github.com/coreos-inc/alm/apis/subscription/v1alpha1"
 	"github.com/coreos-inc/alm/appcache"
 )
 
@@ -21,54 +19,18 @@ import (
 
 // SubscriptionController to use for handling subscriptionv1alpha1.Subscription resource events
 type SubscriptionController struct {
-	catalog   appcache.AppCache
-	client    client.Interface
-	installer Installer
+	catalog appcache.AppCache
+	client  client.Interface
 }
 
-// TEMP - to be filled in with k8s and logic
-type Installer interface {
-	CheckForInstallPlan(namespace string, sub subscriptionv1alpha1.Subscription) (bool, error)
-	CreateInstallPlan(namespace string, plan *installplanv1alpha1.InstallPlan) error
+// HandleSubscription is the handler in the subscriptionv1alpha1.Subscription controller that checks if an
+// Installplan object exists in the namespace for a given Subscription and creates one if not
+func (ctl *SubscriptionController) HandleNewSubscription(sub subscriptionv1alpha1.Subscription) error {
+	return errors.new("Method not implemented")
 }
 
-// // installplanForSubscription is a helper method that fetches the install plan for a
-// // given Subscription from the catalog and installs it into the proper namespace
-// func (ctl *SubscriptionController) installplanForSubscription(sub subscriptionv1alpha1.Subscription) (*opverv1alpha1.OperatorVersion, error) {
-// 	plan, err := ctl.catalog.FindCloudServiceVersionByName(fmt.Sprintf("%s:%s", sub.Spec.AppType, sub.Status.CurrentVersion))
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return &plan, ctl.installer.CreateInstallplan(sub.GetNamespace(), plan)
-// }
-
-// // HandleSubscription is the handler in the subscriptionv1alpha1.Subscription controller that checks if an
-// // Installplan object exists in the namespace for a given Subscription and creates one if not
-// func (ctl *SubscriptionController) HandleNewSubscription(sub subscriptionv1alpha1.Subscription) error {
-// 	ok, err := ctl.installer.CheckForInstallplan(sub.GetNamespace(), sub)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	if ok {
-// 		return nil
-// 	}
-// 	_, err = ctl.installplanForSubscription(sub)
-// 	return err
-// }
-
-// // CheckCatalogForUpdate polls catalog for most recent version of an app and creates a new
-// // Installplan is a more recent version exists
-// func (ctl *SubscriptionController) CheckCatalogForUpdate(sub subscriptionv1alpha1.Subscription) error {
-// 	versionStr, err := ctl.catalog.FindCloudServiceVersionByName(
-// 		fmt.Sprintf("%s:%s", sub.Spec.AppType, sub.Status.Channel))
-// 	if err != nil {
-// 		return err
-// 	}
-// 	ver := semver.New(versionStr)
-// 	currVer := semver.New(sub.Status.CurrentVersion)
-// 	if !currVer.LessThan(*ver) {
-// 		return nil
-// 	}
-// 	_, err = ctl.installplanForSubscription(sub)
-// 	return err
-// }
+// CheckCatalogForUpdate polls catalog for most recent version of an app and creates a new
+// Installplan is a more recent version exists
+func (ctl *SubscriptionController) CheckCatalogForUpdate(sub subscriptionv1alpha1.Subscription) error {
+	return errors.new("Method not implemented")
+}
--- subscriptions/operator_test.go
+++ subscriptions/operator_test.go
@@ -1,57 +1,9 @@
-package catalog
+package subscriptions
 
 import (
-	"fmt"
 	"testing"
-
-	installplanv1alpha1 "github.com/coreos-inc/alm/apis/installplan/v1alpha1"
-
-	"github.com/coreos/go-semver/semver"
 )
 
-// memCatalog is a rough mock catalog that holds apps and declarations in a map
-type memCatalog struct {
-	versions     map[string]semver.Versions
-	declarations map[string]map[string]installplanv1alpha1.InstallPlan
-}
-
-func latest(verlist semver.Versions) (*semver.Version, bool) {
-	if verlist.Len() < 1 {
-		return nil, false
-	}
-	semver.Sort(verlist)
-	return verlist[verlist.Len()-1], true
-}
-
-func (cat *memCatalog) FetchLatestVersion(apptype string) (*semver.Version, error) {
-	versions, ok := cat.versions[apptype]
-	if !ok {
-		return nil, fmt.Errorf("unknown apptype: %s", apptype)
-	}
-	ver, ok := latest(versions)
-	if !ok {
-		return nil, fmt.Errorf("cannot find valid version for apptype %s", apptype)
-	}
-	return ver, nil
-}
-
-func (cat *memCatalog) FetchInstallPlanForAppVersion(apptype, version string) (*installplanv1alpha1.InstallPlan, error) {
-	appversions, ok := cat.declarations[apptype]
-	if !ok {
-		return nil, fmt.Errorf("unknown apptype: %s", apptype)
-	}
-	decl, ok := appversions[version]
-	if !ok {
-		return nil, fmt.Errorf("unknown version %s for app: %s", version, apptype)
-	}
-	return &decl, nil
-}
-
-func (cat *memCatalog) ResolveDependencies(decl *installplanv1alpha1.InstallPlan) error {
-	// you don't get no dependencies!
-	return nil
-}
-
-func TestCatalog(t *testing.T) {
+func TestSubscriptions(t *testing.T) {
 	return
 }
