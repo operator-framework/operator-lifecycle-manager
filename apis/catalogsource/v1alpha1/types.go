fab220e7e1a8f607d79be871e398a221a12755f3
EvB <erica.vonbuelow@coreos.com>
2017-10-03 13:22:15 -0400

    feat(catalogsource): update spec for console to consume

--- apis/catalogsource/v1alpha1/types.go
+++ apis/catalogsource/v1alpha1/types.go
@@ -6,16 +6,44 @@ const (
 	CatalogSourceCRDName       = "apptype-v1s.app.coreos.com"
 	CatalogSourceCRDAPIVersion = "apiextensions.k8s.io/v1beta1" // API version w/ CRD support
 
+	SourceTypeInMem       = "in-mem"
+	SourceTypeConfigMap   = "configmap"
 	SourceTypeOmaha       = "omaha"
 	SourceTypeAppRegistry = "registry"
 )
 
 // CatalogSourceSpec defines a remote (or in-cluster) source for application packages
 type CatalogSourceSpec struct {
-	SourceType string `json:"sourceType"`
-	URL        string `json:"url"`
-	Name       string `json:"name"`
-	SecretName string `json:"secretName"`
+	// Identifying information
+	SourceType string `json:"sourceType"` // only SourceTypeInMem or SourceTypeConfigMap supported
+	Name       string `json:"name"`       // short name, should be unique to source
+
+	// Display information - used in console on catalog source page
+	DisplayName string  `json:"displayName"`
+	Description string  `json:"description"`
+	Sources     []Links `json:"sources"`  // e.g. quay.io/applications/coreos, github.com/k8s/charts
+	Provider    string  `json:"provider"` // catalog publishers, e.g. "Salesforce", "BigCo Internal"
+	Icon        Icon    `json:"icon"`
+
+	// SourceType-specific configuration
+
+	// SourceTypeConfigMap specific fields
+	ConfigMaps []CatalogConfigMaps `json:"configMaps,omitempty"`
+}
+
+type Icon struct {
+	Data      string `json:"base64data"`
+	MediaType string `json:"mediatype"`
+}
+
+type Links struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type CatalogConfigMaps struct {
+	LabelSelectors []metav1.LabelSelector `json:"labelSelectors"`
+	Namespace      string                 `json:"namespace"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
