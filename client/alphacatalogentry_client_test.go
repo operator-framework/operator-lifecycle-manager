1a3c037368a6f26693fb90b1eed368a2fad2519e
EvB <erica.vonbuelow@coreos.com>
2017-10-26 13:29:38 -0400

    wip

--- client/alphacatalogentry_client_test.go
+++ client/alphacatalogentry_client_test.go
@@ -20,12 +20,7 @@ import (
 	csvv1alpha1 "github.com/coreos-inc/alm/apis/clusterserviceversion/v1alpha1"
 )
 
-// func mockClient(t *testing.T, ts *httptest.Server) *AlphaCatalogEntryClient {
-// }
-func testUpdateEntry(t *testing.T, testHandler http.Handler, testEntry *v1alpha1.AlphaCatalogEntry,
-	expectedEntry *v1alpha1.AlphaCatalogEntry, expectedError error) {
-	ts := httptest.NewServer(testHandler)
-	defer ts.Close()
+func mockClient(t *testing.T, ts *httptest.Server) *AlphaCatalogEntryClient {
 	config := rest.Config{
 		Host: ts.URL,
 	}
@@ -43,46 +38,44 @@ func testUpdateEntry(t *testing.T, testHandler http.Handler, testEntry *v1alpha1
 	restClient, err := rest.RESTClientFor(&config)
 	assert.NoError(t, err)
 	assert.NotNil(t, restClient)
-
-	mockClient := &AlphaCatalogEntryClient{
+	return &AlphaCatalogEntryClient{
 		RESTClient: restClient,
 	}
-
-	_, err = mockClient.UpdateEntry(testEntry)
-	actualEntry, err := mockClient.UpdateEntry(testEntry)
-	assert.Equal(t, expectedError, err)
-
-	assert.True(t,
-		equality.Semantic.DeepEqual(expectedEntry, actualEntry),
-		"AlphaCatalogEntry does not match expected value: %s",
-		diff.ObjectDiff(expectedEntry, actualEntry),
-	)
 }
 
-func TestUpdateEntry(t *testing.T) {
+// func mockK8sAPI(t *testing.T, namespace, entryname string) {
+// 	url := fmt.Sprintf("/apis/%s/namespaces/%s/%s/%s",
+// 		v1alpha1.SchemeGroupVersion, namespace, v1alpha1.AlphaCatalogEntryKind, entryname,
+// 	)
 
-	testCSVName := "MockServiceName-v1"
-	testCSVVersion := "0.2.4+alpha"
-
-	testEntry := &v1alpha1.AlphaCatalogEntry{
+// 	mux := http.NewServeMux()
+// 	mux.HandleFunc("/apis/", func(w http.ResponseWriter, req *http.Request) {
+// 	}
+// }
+func createTestEntry(name, version string, labels map[string]string) *v1alpha1.AlphaCatalogEntry {
+	return &v1alpha1.AlphaCatalogEntry{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       v1alpha1.AlphaCatalogEntryKind,
 			APIVersion: v1alpha1.AlphaCatalogEntryCRDAPIVersion,
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      testCSVName,
+			Name:      name,
 			Namespace: "alm-coreos-tests",
+			Labels:    labels,
 		},
 		Spec: &v1alpha1.AlphaCatalogEntrySpec{
 			ClusterServiceVersionSpec: csvv1alpha1.ClusterServiceVersionSpec{
-				Version: *semver.New(testCSVVersion),
-				CustomResourceDefinitions: csvv1alpha1.CustomResourceDefinitions{
-					Owned:    []csvv1alpha1.CRDDescription{},
-					Required: []csvv1alpha1.CRDDescription{},
-				},
+				Version: *semver.New(version),
 			},
 		},
 	}
+}
+func TestUpdateEntry(t *testing.T) {
+
+	testCSVName := "MockServiceName-v1"
+	testCSVVersion := "0.2.4+alpha"
+	testEntry := createTestEntry(testCSVName, testCSVVersion, map[string]string{"test": "v1"})
+
 	expectedEntry := &v1alpha1.AlphaCatalogEntry{Spec: testEntry.Spec}
 	expectedEntry.SetNamespace("alm-coreos-tests-other")
 	rawResp, err := json.Marshal(expectedEntry)
@@ -91,54 +84,67 @@ func TestUpdateEntry(t *testing.T) {
 
 	testHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
+		assert.True(t, false, r.URL.String())
 		w.Write(rawResp)
 	})
-	testUpdateEntry(t, testHandler, testEntry, expectedEntry, nil)
 
-	testHandler2 := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodPost:
-			w.WriteHeader(http.StatusConflict)
-		case http.MethodGet:
-			w.Header().Set("Content-Type", "application/json")
-			w.Write(rawResp)
-		case http.MethodPut:
-			w.Header().Set("Content-Type", "application/json")
-			w.Write(rawResp)
-		}
-	})
-	testUpdateEntry(t, testHandler2, testEntry, expectedEntry, nil)
+	ts := httptest.NewServer(testHandler)
+	defer ts.Close()
 
-	testHandler3 := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodPost:
-			w.WriteHeader(http.StatusConflict)
-		case http.MethodGet:
-			w.WriteHeader(http.StatusInternalServerError)
-		case http.MethodPut:
-			w.Header().Set("Content-Type", "application/json")
-			w.Write(rawResp)
-		}
-	})
-	testUpdateEntry(t, testHandler3, testEntry, nil,
-		fmt.Errorf("failed to find then update AlphaCatalogEntry: failed to update CR status: "+
-			"an error on the server (\"\") has prevented the request from succeeding (get %s.%s %s)",
-			v1alpha1.AlphaCatalogEntryCRDName, "app.coreos.com", testCSVName))
+	actualEntry, err := mockClient(t, ts).UpdateEntry(testEntry)
+	assert.Equal(t, nil, err) //expectedError, err)
 
-	testHandler4 := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodPost:
-			w.WriteHeader(http.StatusConflict)
-		case http.MethodGet:
-			w.Header().Set("Content-Type", "application/json")
-			w.Write(rawResp)
-		case http.MethodPut:
-			w.WriteHeader(http.StatusInternalServerError)
-		}
-	})
-	testUpdateEntry(t, testHandler4, testEntry, nil,
-		fmt.Errorf("failed to update AlphaCatalogEntry: "+
-			"an error on the server (\"\") has prevented the request from succeeding (put %s.%s %s)",
-			v1alpha1.AlphaCatalogEntryCRDName, "app.coreos.com", testCSVName))
+	assert.True(t,
+		equality.Semantic.DeepEqual(expectedEntry, actualEntry),
+		"AlphaCatalogEntry does not match expected value: %s",
+		diff.ObjectDiff(expectedEntry, actualEntry),
+	)
+	// testUpdateEntry(t, testHandler, testEntry, expectedEntry, nil)
+
+	// testHandler2 := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 	switch r.Method {
+	// 	case http.MethodPost:
+	// 		w.WriteHeader(http.StatusConflict)
+	// 	case http.MethodGet:
+	// 		w.Header().Set("Content-Type", "application/json")
+	// 		w.Write(rawResp)
+	// 	case http.MethodPut:
+	// 		w.Header().Set("Content-Type", "application/json")
+	// 		w.Write(rawResp)
+	// 	}
+	// })
+	// testUpdateEntry(t, testHandler2, testEntry, expectedEntry, nil)
+
+	// testHandler3 := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 	switch r.Method {
+	// 	case http.MethodPost:
+	// 		w.WriteHeader(http.StatusConflict)
+	// 	case http.MethodGet:
+	// 		w.WriteHeader(http.StatusInternalServerError)
+	// 	case http.MethodPut:
+	// 		w.Header().Set("Content-Type", "application/json")
+	// 		w.Write(rawResp)
+	// 	}
+	// })
+	// testUpdateEntry(t, testHandler3, testEntry, nil,
+	// 	fmt.Errorf("failed to find then update AlphaCatalogEntry: failed to update CR status: "+
+	// 		"an error on the server (\"\") has prevented the request from succeeding (get %s.%s %s)",
+	// 		v1alpha1.AlphaCatalogEntryCRDName, "app.coreos.com", testCSVName))
+
+	// testHandler4 := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 	switch r.Method {
+	// 	case http.MethodPost:
+	// 		w.WriteHeader(http.StatusConflict)
+	// 	case http.MethodGet:
+	// 		w.Header().Set("Content-Type", "application/json")
+	// 		w.Write(rawResp)
+	// 	case http.MethodPut:
+	// 		w.WriteHeader(http.StatusInternalServerError)
+	// 	}
+	// })
+	// testUpdateEntry(t, testHandler4, testEntry, nil,
+	// 	fmt.Errorf("failed to update AlphaCatalogEntry: "+
+	// 		"an error on the server (\"\") has prevented the request from succeeding (put %s.%s %s)",
+	// 		v1alpha1.AlphaCatalogEntryCRDName, "app.coreos.com", testCSVName))
 
 }
