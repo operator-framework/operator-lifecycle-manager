f2800f0ce947ade5c7c6276885d1937afcd36dff
Jake Moshenko <jake.moshenko@coreos.com>
2017-12-05 14:19:25 -0500

    Fixes for the Catalog configmap generator

---
+++ build_catalog_configmap.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+OUTFILE=${1:-'catalog_resources/tectonicocs.configmap.yaml'}
+NAMESPACE=${2:-tectonic-system}
+
+cat <<EOF > $OUTFILE
+kind: ConfigMap
+apiVersion: v1
+metadata:
+  name: tectonic-ocs
+  namespace: $NAMESPACE
+  labels:
+    tectonic-operators.coreos.com/managed-by: tectonic-x-operator
+data:
+  customResourceDefinitions: |-
+EOF
+
+for crd in catalog_resources/*.crd.yaml
+do
+  printf "    - " >> $OUTFILE
+  head -n 1 $crd >> $OUTFILE
+  tail -n +2 $crd | sed 's/^/      /' >> $OUTFILE
+done
+
+printf '  clusterServiceVersions: |-\n' >> $OUTFILE
+
+for csv in catalog_resources/*.clusterserviceversion.yaml
+do
+  printf "    - " >> $OUTFILE
+  head -n 3 $csv | tail -n 1 >> $OUTFILE
+  tail -n +4 $csv | sed 's/^/      /' >> $OUTFILE
+done
