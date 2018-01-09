b689a5df1f70a9f1f98db500972c7df25cf85582
EvB <erica.vonbuelow@coreos.com>
2018-01-09 15:23:53 -0500

    feat(apis/subscription): add subscription crd back in

---
+++ pkg/apis/subscription/v1alpha1/types.go
@@ -0,0 +1,58 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	GroupVersion              = "v1alpha1" // version used in the Scheme for subscriptions
+	SubscriptionKind          = "Subscription-v1"
+	SubscriptionListKind      = "SubscriptionList-v1"
+	SubscriptionCRDName       = "subscription-v1s.app.coreos.com"
+	SubscriptionCRDAPIVersion = "apiextensions.k8s.io/v1beta1" // API version w/ CRD support
+)
+
+// SubscriptionState tracks when updates are available, installing, or service is up to date
+type SubscriptionState string
+
+const (
+	SubscriptionStateNone             = ""
+	SubscriptionStateUpgradeAvailable = "UpgradeAvailable"
+	SubscriptionStateUpgradePending   = "UpgradePending"
+	SubscriptionStateAtLatest         = "AtLatestKnown"
+)
+
+// SubscriptionSpec defines an Application that can be installed
+type SubscriptionSpec struct {
+	CatalogSource string `json:"source"`
+	Package       string `json:"name"`
+	Channel       string `json:"channel,omitempty"`
+	LatestCSV     string `json:"latestCSV,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// Custom Resource of type "SubscriptionSpec"
+type Subscription struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata"`
+
+	Spec   *SubscriptionSpec  `json:"spec"`
+	Status SubscriptionStatus `json:"status"`
+}
+
+type SubscriptionStatus struct {
+	metav1.Status `json:",inline"`
+
+	InstalledCSVName string            `json:"installedCSV"`
+	State            SubscriptionState `json:"state"`
+	LastUpdated      metav1.Time       `json:"lastUpdated"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type SubscriptionList struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata"`
+
+	Items []*SubscriptionSpec `json:"items"`
+}
