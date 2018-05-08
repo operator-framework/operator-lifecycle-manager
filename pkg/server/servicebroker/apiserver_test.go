f2f984780eec8f4e262e40a2c576b8422d7c3d4b
EvB <erica.vonbuelow@coreos.com>
2018-05-08 14:46:20 -0400

    fix(servicebroker): reverse map check bool order

--- pkg/server/servicebroker/apiserver_test.go
+++ pkg/server/servicebroker/apiserver_test.go
@@ -3,28 +3,12 @@ package servicebroker
 import (
 	"testing"
 
-	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
-
-	"github.com/operator-framework/operator-lifecycle-manager/pkg/api/client/clientset/versioned/fake"
 )
-
-const (
-	testNamespace = "testns"
-)
-
-func mockALMBroker(ctrl *gomock.Controller) *ALMBroker {
-	return &ALMBroker{
-		client:    fake.NewSimpleClientset(),
-		namespace: testNamespace,
-	}
-}
 
 func TestValidateBrokerAPIVersion(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
 	// test bad version
-	brokerMock := mockALMBroker(ctrl)
+	brokerMock := &ALMBroker{}
 	err := brokerMock.ValidateBrokerAPIVersion("oops")
 	require.EqualError(t, err, "unknown OpenServiceBroker API Version: oops")
 
