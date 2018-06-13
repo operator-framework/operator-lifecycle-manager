7061310f3061e1ec5aa2afc71fff6f08047dedd5
Evan Cordell <cordell.evan@gmail.com>
2018-06-13 11:12:58 -0400

    chore(deps): remove tectonic-operators as a dependency

--- vendor/github.com/coreos-inc/tectonic-operators/operator-client/pkg/types/config_map.go
+++
@@ -1,7 +0,0 @@
-package types
-
-import "k8s.io/api/core/v1"
-
-// ConfigMapModifier is a modifier function to be used when atomically
-// updating a ConfigMap.
-type ConfigMapModifier func(*v1.ConfigMap) error
--- vendor/github.com/coreos-inc/tectonic-operators/operator-client/pkg/types/node.go
+++
@@ -1,7 +0,0 @@
-package types
-
-import "k8s.io/api/core/v1"
-
-// NodeModifier is a function that can modify a
-// Node during an atomic update.
-type NodeModifier func(*v1.Node) error
