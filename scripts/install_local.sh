ae8352b3617f96e2562041c4ad98dfd340ec248d
Evan Cordell <cordell.evan@gmail.com>
2018-07-31 18:13:32 -0400

    fix(catalogs): update catalog data to use ClusterServiceVersion

--- scripts/install_local.sh
+++ scripts/install_local.sh
@@ -39,11 +39,3 @@ do
   n=$[$n+1]
   sleep 1
 done
-
-n=0
-until [ $n -ge 5 ]
-do
-  kubectl rollout status -w deployment/alm-service-broker --namespace=${namespace} && break
-  n=$[$n+1]
-  sleep 1
-done
