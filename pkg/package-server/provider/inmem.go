a55524078ff869354ad26c8bf133f17d5b364dcb
njhale <njohnhale@gmail.com>
2018-09-10 19:54:13 -0400

    feat(inmem): collect CSV data in PackageManifestStatus

--- pkg/package-server/provider/inmem.go
+++ pkg/package-server/provider/inmem.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"sync"
+	"time"
 
 	"github.com/ghodss/yaml"
 	log "github.com/sirupsen/logrus"
@@ -17,7 +18,13 @@ import (
 	packagev1alpha1 "github.com/operator-framework/operator-lifecycle-manager/pkg/package-server/apis/packagemanifest/v1alpha1"
 )
 
-const ConfigMapPackageName = "packages"
+const (
+	// ConfigMapPackageName is the key for package ConfigMap data
+	ConfigMapPackageName = "packages"
+
+	// ConfigMapCSVName is the key for CSV ConfigMap data
+	ConfigMapCSVName = "clusterServiceVersions"
+)
 
 type packageKey struct {
 	catalogSourceName      string
@@ -25,6 +32,7 @@ type packageKey struct {
 	packageName            string
 }
 
+// InMemoryProvider syncs and provides PackageManifests from the cluster in an in-memory cache
 type InMemoryProvider struct {
 	*queueinformer.Operator
 
@@ -32,6 +40,7 @@ type InMemoryProvider struct {
 	manifests map[packageKey]packagev1alpha1.PackageManifest
 }
 
+// NewInMemoryProvider returns a pointer to a new InMemoryProvider instance
 func NewInMemoryProvider(informers []cache.SharedIndexInformer, queueOperator *queueinformer.Operator) *InMemoryProvider {
 	// instantiate the in-mem provider
 	prov := &InMemoryProvider{
@@ -55,9 +64,8 @@ func NewInMemoryProvider(informers []cache.SharedIndexInformer, queueOperator *q
 	return prov
 }
 
-func loadPackageManifestsFromConfigMap(cm *corev1.ConfigMap, catalogSourceName, catalogSourceNamespace string) ([]packagev1alpha1.PackageManifest, error) {
-	var manifests []packagev1alpha1.PackageManifest
-
+// parsePackageManifestsFromConfigMap returns a list of PackageManifests from a given ConfigMap
+func parsePackageManifestsFromConfigMap(cm *corev1.ConfigMap, catalogSourceName, catalogSourceNamespace string) ([]packagev1alpha1.PackageManifest, error) {
 	cmName := cm.GetName()
 	logger := log.WithFields(log.Fields{
 		"Action": "Load ConfigMap",
@@ -65,17 +73,44 @@ func loadPackageManifestsFromConfigMap(cm *corev1.ConfigMap, catalogSourceName, 
 	})
 
 	found := false
+	csvs := make(map[string]operatorsv1alpha1.ClusterServiceVersion)
+	csvListYaml, ok := cm.Data[ConfigMapCSVName]
+	if ok {
+		logger.Debug("ConfigMap contains CSVsf")
+		csvListJSON, err := yaml.YAMLToJSON([]byte(csvListYaml))
+		if err != nil {
+			log.Debugf("Load ConfigMap     -- ERROR %s : error=%s", cmName, err)
+			return nil, fmt.Errorf("error loading CSV list yaml from ConfigMap %s: %s", cmName, err)
+		}
+
+		var parsedCSVList []operatorsv1alpha1.ClusterServiceVersion
+		err = json.Unmarshal([]byte(csvListJSON), &parsedCSVList)
+		if err != nil {
+			log.Debugf("Load ConfigMap     -- ERROR %s : error=%s", cmName, err)
+			return nil, fmt.Errorf("error parsing CSV list (json) from ConfigMap %s: %s", cmName, err)
+		}
+
+		for _, csv := range parsedCSVList {
+			found = true
+
+			// TODO: add check for invalid CSV definitions
+			log.Debugf("found csv %s", csv.GetName())
+			csvs[csv.GetName()] = csv
+		}
+	}
+
+	manifests := []packagev1alpha1.PackageManifest{}
 	packageListYaml, ok := cm.Data[ConfigMapPackageName]
 	if ok {
 		logger.Debug("ConfigMap contains packages")
-		packageListJson, err := yaml.YAMLToJSON([]byte(packageListYaml))
+		packageListJSON, err := yaml.YAMLToJSON([]byte(packageListYaml))
 		if err != nil {
 			logger.Debugf("ERROR: %s", err)
 			return nil, fmt.Errorf("error loading package list yaml from ConfigMap %s: %s", cmName, err)
 		}
 
 		var parsedStatuses []packagev1alpha1.PackageManifestStatus
-		err = json.Unmarshal([]byte(packageListJson), &parsedStatuses)
+		err = json.Unmarshal([]byte(packageListJSON), &parsedStatuses)
 		if err != nil {
 			logger.Debugf("ERROR: %s", err)
 			return nil, fmt.Errorf("error parsing package list (json) from ConfigMap %s: %s", cmName, err)
@@ -85,20 +120,47 @@ func loadPackageManifestsFromConfigMap(cm *corev1.ConfigMap, catalogSourceName, 
 			found = true
 
 			// add the name and namespace of the CatalogSource
-			status.CatalogSourceName = catalogSourceName
-			status.CatalogSourceNamespace = catalogSourceNamespace
 			manifest := packagev1alpha1.PackageManifest{
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      status.PackageName,
 					Namespace: cm.GetNamespace(),
+					Labels:    map[string]string{},
 				},
 				Status: status,
 			}
 
-			// TODO: add check for invalid package definitions
+			manifest.Status.CatalogSourceName = catalogSourceName
+			manifest.Status.CatalogSourceNamespace = catalogSourceNamespace
+
+			// add all PackageChannel CSVDescriptions
+			for i, channel := range manifest.Status.Channels {
+				csv, ok := csvs[channel.CurrentCSVName]
+				if !ok {
+					return nil, fmt.Errorf("packagemanifest %s references non-existent csv %s", manifest.Status.PackageName, channel.CurrentCSVName)
+				}
+
+				manifest.Status.Channels[i].CurrentCSVDesc = packagev1alpha1.CreateCSVDescription(&csv)
+
+				// set the Provider
+				if manifest.Status.DefaultChannelName != "" && csv.GetName() == manifest.Status.DefaultChannelName || i == 0 {
+					manifest.Status.Provider = packagev1alpha1.AppLink{
+						Name: csv.Spec.Provider.Name,
+						URL:  csv.Spec.Provider.URL,
+					}
+
+					// add Provider as a label
+					manifest.ObjectMeta.Labels["provider"] = manifest.Status.Provider.Name
+					manifest.ObjectMeta.Labels["provider-url"] = manifest.Status.Provider.URL
+				}
+			}
+
+			// set CatalogSource labels
+			manifest.ObjectMeta.Labels["catalog"] = manifest.Status.CatalogSourceName
+			manifest.ObjectMeta.Labels["catalog-namespace"] = manifest.Status.CatalogSourceNamespace
+
+			log.Debugf("retrieved packagemanifest %s", manifest.GetName())
 			manifests = append(manifests, manifest)
 		}
-		logger.Debugf("Found packages: %v", manifests)
 	}
 
 	if !found {
@@ -110,7 +172,7 @@ func loadPackageManifestsFromConfigMap(cm *corev1.ConfigMap, catalogSourceName, 
 }
 
 func (m *InMemoryProvider) syncCatalogSource(obj interface{}) error {
-	// assert as catalog source
+	// assert as CatalogSource
 	catsrc, ok := obj.(*operatorsv1alpha1.CatalogSource)
 	if !ok {
 		log.Debugf("wrong type: %#v", obj)
@@ -119,17 +181,17 @@ func (m *InMemoryProvider) syncCatalogSource(obj interface{}) error {
 
 	var manifests []packagev1alpha1.PackageManifest
 
-	// check if the sourcetype is internal
+	// handle by sourceType
 	switch catsrc.Spec.SourceType {
 	case "internal":
-		// get the catalog source's config map
+		// get the CatalogSource's ConfigMap
 		cm, err := m.OpClient.KubernetesInterface().CoreV1().ConfigMaps(catsrc.GetNamespace()).Get(catsrc.Spec.ConfigMap, metav1.GetOptions{})
 		if err != nil {
 			return fmt.Errorf("failed to get catalog config map %s when updating status: %s", catsrc.Spec.ConfigMap, err)
 		}
 
-		// load the package manifest from the config map
-		manifests, err = loadPackageManifestsFromConfigMap(cm, catsrc.GetName(), catsrc.GetNamespace())
+		// parse PackageManifest from ConfigMap
+		manifests, err = parsePackageManifestsFromConfigMap(cm, catsrc.GetName(), catsrc.GetNamespace())
 		if err != nil {
 			return fmt.Errorf("failed to load package manifest from config map %s", cm.GetName())
 		}
@@ -138,15 +200,24 @@ func (m *InMemoryProvider) syncCatalogSource(obj interface{}) error {
 		return fmt.Errorf("catalog source %s in namespace %s source type %s not recognized", catsrc.GetName(), catsrc.GetNamespace(), catsrc.Spec.SourceType)
 	}
 
-	// update package manifests
+	// update manifests
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	for _, manifest := range manifests {
 		key := packageKey{
-			catalogSourceName:      catsrc.GetName(),
-			catalogSourceNamespace: catsrc.GetNamespace(),
-			packageName:            manifest.Status.PackageName,
+			catalogSourceName:      manifest.Status.CatalogSourceName,
+			catalogSourceNamespace: manifest.Status.CatalogSourceNamespace,
+			packageName:            manifest.GetName(),
 		}
+
+		if pm, ok := m.manifests[key]; ok {
+			// use existing CreationTimestamp
+			manifest.CreationTimestamp = pm.ObjectMeta.CreationTimestamp
+		} else {
+			// set CreationTimestamp if first time seeing the PackageManifest
+			manifest.CreationTimestamp = metav1.NewTime(time.Now())
+		}
+
 		log.Debugf("storing packagemanifest at %+v", key)
 		m.manifests[key] = manifest
 	}
@@ -165,6 +236,7 @@ func (m *InMemoryProvider) ListPackageManifests(namespace string) (*packagev1alp
 		var matching []packagev1alpha1.PackageManifest
 		for _, manifest := range m.manifests {
 			if manifest.GetNamespace() == namespace {
+				// tack on the csv spec for each channel
 				matching = append(matching, manifest)
 			}
 		}
@@ -180,7 +252,7 @@ func (m *InMemoryProvider) GetPackageManifest(namespace, name string) (*packagev
 	m.mu.RLock()
 	defer m.mu.RUnlock()
 
-	manifest := packagev1alpha1.PackageManifest{}
+	var manifest packagev1alpha1.PackageManifest
 	for key, pm := range m.manifests {
 		if key.packageName == name && key.catalogSourceNamespace == namespace {
 			manifest = pm
