7f25bfa232571e8a66172aab0361d90caabb3997
njhale <njohnhale@gmail.com>
2018-09-10 08:19:01 -0400

    test(packagemanifest): add fetch function for packagemanifests

--- test/e2e/packagemanifest_e2e_test.go
+++ test/e2e/packagemanifest_e2e_test.go
@@ -7,11 +7,40 @@ import (
 	"github.com/stretchr/testify/require"
 	extv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
 
 	"github.com/operator-framework/operator-lifecycle-manager/pkg/api/apis/operators/v1alpha1"
 	"github.com/operator-framework/operator-lifecycle-manager/pkg/controller/registry"
 	pmv1alpha1 "github.com/operator-framework/operator-lifecycle-manager/pkg/package-server/apis/packagemanifest/v1alpha1"
+	pmversioned "github.com/operator-framework/operator-lifecycle-manager/pkg/package-server/client/clientset/versioned"
 )
+
+type packageManifestCheckFunc func(*pmv1alpha1.PackageManifest) bool
+
+func packageManifestHasStatus(pm *pmv1alpha1.PackageManifest) bool {
+	// as long as it has a package name we consider the status non-empty
+	if pm == nil || pm.Status.PackageName == "" {
+		return false
+	}
+
+	return true
+}
+
+func fetchPackageManifest(t *testing.T, pmc pmversioned.Interface, namespace, name string, check packageManifestCheckFunc) (*pmv1alpha1.PackageManifest, error) {
+	var fetched *pmv1alpha1.PackageManifest
+	var err error
+
+	err = wait.Poll(pollInterval, pollDuration, func() (bool, error) {
+		t.Logf("Polling...")
+		fetched, err = pmc.Packagemanifest().PackageManifests(namespace).Get(name, metav1.GetOptions{})
+		if err != nil {
+			return true, err
+		}
+		return check(fetched), nil
+	})
+
+	return fetched, err
+}
 
 func TestPackageManifestLoading(t *testing.T) {
 	// create a simple catalogsource
@@ -52,13 +81,12 @@ func TestPackageManifestLoading(t *testing.T) {
 		DefaultChannelName: stableChannel,
 	}
 
-	// TODO: Ensure catalog source is tracked by package server before checking
+	// get PackageManifest
 	pmc := newPMClient(t)
-	pm, err := pmc.Packagemanifest().PackageManifests(testNamespace).Get(packageName, metav1.GetOptions{})
+	pm, err := fetchPackageManifest(t, pmc, testNamespace, packageName, packageManifestHasStatus)
 
-	// check parsed PackageManifest
+	// check against expected
 	require.NoError(t, err, "error getting package manifest")
-	t.Logf("packagemanifest: %v", pm)
 	require.NotNil(t, pm)
 	require.Equal(t, packageName, pm.GetName())
 	require.Equal(t, expectedStatus, pm.Status)
