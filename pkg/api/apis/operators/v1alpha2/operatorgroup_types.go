8d4563e175d0d0fe52511f1c5e8bf64fe7be3696
Jeff Peeler <jpeeler@redhat.com>
2018-09-19 18:37:12 -0400

    feat(olm): add OperatorGroup types
    This includes strictly the non-generated code.

---
+++ pkg/api/apis/operators/v1alpha2/operatorgroup_types.go
@@ -0,0 +1,34 @@
+package v1alpha2
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type OperatorGroupSpec struct {
+	Selector       metav1.LabelSelector  `json:"selector,omitempty"`
+	ServiceAccount corev1.ServiceAccount `json:"serviceAccount,omitempty"`
+}
+
+type OperatorGroupStatus struct {
+	Namespaces  []corev1.Namespace `json:"namespaces"`
+	LastUpdated metav1.Time        `json:"lastUpdated"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +genclient
+type OperatorGroup struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata"`
+
+	Spec   OperatorGroupSpec   `json:"spec"`
+	Status OperatorGroupStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type OperatorGroupList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+
+	Items []OperatorGroup `json:"items"`
+}
