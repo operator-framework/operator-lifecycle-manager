ecafeca0bd9f6ba8b2bde132e20e28aa425eab58
njhale <njohnhale@gmail.com>
2018-11-07 16:42:05 -0500

    fix(logging): use structure logging in checkAPIServiceResources

--- scripts/build_local.sh
+++ scripts/build_local.sh
@@ -6,7 +6,7 @@
 set -e
 
 if [ -z "$NO_MINIKUBE" ]; then
-  ps x | grep -q [m]inikube || minikube start --kubernetes-version="v1.12.0" --extra-config=apiserver.v=4 || { echo 'Cannot start minikube.'; exit 1; }
+  ps x | grep -q [m]inikube || minikube start --kubernetes-version="v1.11.0" --extra-config=apiserver.v=4 || { echo 'Cannot start minikube.'; exit 1; }
   eval $(minikube docker-env) || { echo 'Cannot switch to minikube docker'; exit 1; }
   kubectl config use-context minikube
 fi
