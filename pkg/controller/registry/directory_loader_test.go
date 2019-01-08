eaf605cca864e9efc303f29224353d6439b541c5
kevinrizza <krizza@redhat.com>
2019-01-08 09:48:24 -0500

    Remove operatorsource installation
    -Remove operatorsource and default cr templates
    -Remove all operatorsource manifests from latest release folders
    -Remove marketplace from rh-operators
    -Update directory loader test to remove marketplace reference

--- pkg/controller/registry/directory_loader_test.go
+++ pkg/controller/registry/directory_loader_test.go
@@ -22,7 +22,6 @@ func TestDirectoryLoader(t *testing.T) {
 	require.Contains(t, catalog.packages, "federationv2")
 	require.Contains(t, catalog.packages, "prometheus")
 	require.Contains(t, catalog.packages, "svcat")
-	require.Contains(t, catalog.packages, "marketplace")
 }
 
 func TestDirectoryLoaderHiddenDirs(t *testing.T) {
