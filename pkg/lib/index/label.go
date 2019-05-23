2634cb4322150aab3c263b404fa80a70ffda843f
njhale <njohnhale@gmail.com>
2019-05-23 19:12:27 -0400

    refactor(index): reduce label index search to a single cache

--- pkg/lib/index/label.go
+++ pkg/lib/index/label.go
@@ -9,50 +9,48 @@ import (
 )
 
 const (
-	// MetaLabelIndexFuncKey is the recommended key to use for registering the index func with an indexer.
+	// MetaLabelIndexFuncKey is the recommended key to use for registering MetaLabelIndexFunc with an indexer.
 	MetaLabelIndexFuncKey string = "metalabelindexfunc"
 )
 
-// MetaLabelIndexFunc returns indicies from the labels of the given object.
+// MetaLabelIndexFunc returns indices from the labels of the given object.
 func MetaLabelIndexFunc(obj interface{}) ([]string, error) {
-	indicies := []string{}
+	indices := []string{}
 	m, err := meta.Accessor(obj)
 	if err != nil {
-		return indicies, fmt.Errorf("object has no meta: %v", err)
+		return indices, fmt.Errorf("object has no meta: %v", err)
 	}
 
 	for k, v := range m.GetLabels() {
-		indicies = append(indicies, fmt.Sprintf("%s=%s", k, v))
+		indices = append(indices, fmt.Sprintf("%s=%s", k, v))
 	}
 
-	return indicies, nil
+	return indices, nil
 }
 
-// LabelIndexKeys returns the union of indexed cache keys in the given indexers matching the same labels as the given selector
-func LabelIndexKeys(indexers map[string]cache.Indexer, labelSets ...labels.Set) ([]string, error) {
+// LabelIndexKeys returns the union of indexed cache keys in indexer matching the same labels as the given label sets.
+func LabelIndexKeys(indexer cache.Indexer, labelSets ...labels.Set) ([]string, error) {
 	keySet := map[string]struct{}{}
 	keys := []string{}
-	for _, indexer := range indexers {
-		for _, labelSet := range labelSets {
-			for key, value := range labelSet {
-				apiLabelKey := fmt.Sprintf("%s=%s", key, value)
-				cacheKeys, err := indexer.IndexKeys(MetaLabelIndexFuncKey, apiLabelKey)
-				if err != nil {
-					return nil, err
-				}
-
-				for _, cacheKey := range cacheKeys {
-					// Detect duplication
-					if _, ok := keySet[cacheKey]; ok {
-						continue
-					}
-
-					// Add to set
-					keySet[cacheKey] = struct{}{}
-					keys = append(keys, cacheKey)
-				}
-
+	for _, labelSet := range labelSets {
+		for key, value := range labelSet {
+			apiLabelKey := fmt.Sprintf("%s=%s", key, value)
+			cacheKeys, err := indexer.IndexKeys(MetaLabelIndexFuncKey, apiLabelKey)
+			if err != nil {
+				return nil, err
 			}
+
+			for _, cacheKey := range cacheKeys {
+				// Detect duplication
+				if _, ok := keySet[cacheKey]; ok {
+					continue
+				}
+
+				// Add to set
+				keySet[cacheKey] = struct{}{}
+				keys = append(keys, cacheKey)
+			}
+
 		}
 	}
 
