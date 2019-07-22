7ea5b6903742b27919e217488fb3638b0fe5d904
Abu Kashem <akashem@redhat.com>
2019-07-22 16:49:05 -0400

    (feat) requeue the csv when subscription changes
    When a subscription changes, if it has an installed CSV then requeue
    it so that any changes in pod configuration can take effect
    immediately.

--- pkg/controller/operators/olm/operator.go
+++ pkg/controller/operators/olm/operator.go
@@ -453,10 +453,19 @@ func (a *Operator) now() metav1.Time {
 }
 
 func (a *Operator) syncSubscription(obj interface{}) error {
-	_, ok := obj.(*v1alpha1.Subscription)
+	sub, ok := obj.(*v1alpha1.Subscription)
 	if !ok {
 		a.logger.Debugf("wrong type: %#v\n", obj)
 		return fmt.Errorf("casting Subscription failed")
+	}
+
+	installedCSV := sub.Status.InstalledCSV
+	if installedCSV != "" {
+		a.logger.WithField("csv", installedCSV).Debug("subscription has changed, requeuing installed csv")
+		if err := a.csvQueueSet.Requeue(sub.GetNamespace(), installedCSV); err != nil {
+			a.logger.WithField("csv", installedCSV).Debug("failed to requeue installed csv")
+			return err
+		}
 	}
 
 	return nil
