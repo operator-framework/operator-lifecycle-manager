bab509bd05c656a5fcc1787bc1f6d3db7ceeeade
njhale <njohnhale@gmail.com>
2019-07-31 10:27:01 -0400

    feat(operatorsv1alpha1): enable openapi-gen

--- pkg/api/apis/operators/v1alpha1/doc.go
+++ pkg/api/apis/operators/v1alpha1/doc.go
@@ -1,3 +1,4 @@
+// +k8s:openapi-gen=true
 // +k8s:deepcopy-gen=package
 // +k8s:conversion-gen=github.com/operator-framework/operator-lifecycle-manager/pkg/api/apis/operators
 // +groupName=operators.coreos.com
