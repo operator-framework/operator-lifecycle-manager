de4c8e1e56093fa425aaec2b77ec75beb9653d68
awgreene <agreene@redhat.com>
2019-12-03 15:59:35 -0500

    Implement controller-gen
    This commit updates OLM's Makefile to use the controller-gen tool to
    generate the structural schemas for its CRDs.

--- tools.go
+++ tools.go
@@ -5,6 +5,7 @@ package tools
 import (
 	_ "github.com/golang/mock/mockgen"
 	_ "github.com/maxbrunsfeld/counterfeiter/v6"
+	_ "github.com/mikefarah/yq/v2"
 	_ "k8s.io/code-generator/cmd/client-gen"
 	_ "k8s.io/code-generator/cmd/conversion-gen/"
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
@@ -16,4 +17,5 @@ import (
 	_ "k8s.io/code-generator/cmd/openapi-gen"
 	_ "k8s.io/code-generator/cmd/set-gen"
 	_ "k8s.io/kube-openapi/cmd/openapi-gen"
+	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 )
