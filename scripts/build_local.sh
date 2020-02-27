a13695f666e6c2f0cee8f861105e8e11e8fc9ae3
Ben Luddy <bluddy@redhat.com>
2020-02-27 09:27:31 -0500

    Unbreak the minikube mode of build_local.sh.
    Fix the ordering of eval "$(minikube docker-env)" relative to local
    image builds, which was broken in the process of fixing this script
    for kind users.

--- scripts/build_local.sh
+++ scripts/build_local.sh
@@ -5,18 +5,19 @@
 
 set -e
 
-docker build -f local.Dockerfile -t quay.io/operator-framework/olm:local -t quay.io/operator-framework/olm-e2e:local ./bin
-docker build -f test/e2e/hang.Dockerfile -t hang:10 ./bin
-
-if [ -x "$(command -v kind)" ] && [[ "$(kubectl config current-context)" =~ ^kind-? ]]; then
-  kind load docker-image quay.io/operator-framework/olm:local
-  kind load docker-image quay.io/operator-framework/olm-e2e:local
-  kind load docker-image hang:10
-  exit 0 # don't bother with minikube if kind is active
-fi
+[ -x "$(command -v kind)" ] && [[ "$(kubectl config current-context)" =~ ^kind-? ]] && KIND=1 NO_MINIKUBE=1
 
 if [ -z "$NO_MINIKUBE" ]; then
   pgrep -f "[m]inikube" >/dev/null || minikube start --kubernetes-version="v1.16.2" --extra-config=apiserver.v=4 || { echo 'Cannot start minikube.'; exit 1; }
   eval "$(minikube docker-env)" || { echo 'Cannot switch to minikube docker'; exit 1; }
   kubectl config use-context minikube
+fi
+
+docker build -f local.Dockerfile -t quay.io/operator-framework/olm:local -t quay.io/operator-framework/olm-e2e:local ./bin
+docker build -f test/e2e/hang.Dockerfile -t hang:10 ./bin
+
+if [ -n "$KIND" ]; then
+  kind load docker-image quay.io/operator-framework/olm:local
+  kind load docker-image quay.io/operator-framework/olm-e2e:local
+  kind load docker-image hang:10
 fi
