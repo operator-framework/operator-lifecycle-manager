091a2b7dd457598dd89a11f3470f7cbb45c708a9
Nick Hale <njohnhale@gmail.com>
2020-04-06 10:21:04 -0400

    chore(make): copy crd manifests from apis repo

--- tools.go
+++ tools.go
@@ -8,9 +8,9 @@ import (
 	_ "github.com/maxbrunsfeld/counterfeiter/v6"
 	_ "github.com/mikefarah/yq/v2"
 	_ "github.com/onsi/ginkgo/ginkgo"
+	_ "github.com/operator-framework/api/crds" // operators.coreos.com CRD manifests
 	_ "helm.sh/helm/v3/cmd/helm"
 	_ "k8s.io/code-generator"
 	_ "k8s.io/kube-openapi/cmd/openapi-gen"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 )
-
