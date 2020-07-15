7dae71009d700a2235f39f745d7cf1930fb948aa
Evan <cordell.evan@gmail.com>
2020-07-15 19:43:35 -0400

    treat dependencies generically

--- pkg/controller/registry/resolver/util_test.go
+++ pkg/controller/registry/resolver/util_test.go
@@ -45,7 +45,7 @@ func NewGenerationFromOperators(ops ...OperatorSurface) *NamespaceGeneration {
 	return g
 }
 
-func NewFakeOperatorSurface(name, pkg, channel, replaces, src, startingCSV string, providedCRDs, requiredCRDs, providedAPIServices, requiredAPIServices []opregistry.APIKey, dependencies []VersionDependency) *Operator {
+func NewFakeOperatorSurface(name, pkg, channel, replaces, src, startingCSV string, providedCRDs, requiredCRDs, providedAPIServices, requiredAPIServices []opregistry.APIKey, dependencies []*api.Dependency) *Operator {
 	providedAPISet := EmptyAPISet()
 	requiredAPISet := EmptyAPISet()
 	providedCRDAPISet := EmptyAPISet()
@@ -85,7 +85,7 @@ func NewFakeOperatorSurface(name, pkg, channel, replaces, src, startingCSV strin
 			Catalog:     CatalogKey{src, src + "-namespace"},
 		},
 		bundle:              b,
-		versionDependencies: dependencies,
+		dependencies: dependencies,
 	}
 }
 
@@ -212,7 +212,7 @@ func u(object runtime.Object) *unstructured.Unstructured {
 	return &unstructured.Unstructured{Object: unst}
 }
 
-func apiSetToGVk(crds, apis APISet) (out []*api.GroupVersionKind) {
+func apiSetToGVK(crds, apis APISet) (out []*api.GroupVersionKind) {
 	out = make([]*api.GroupVersionKind, 0)
 	for a := range crds {
 		out = append(out, &api.GroupVersionKind{
@@ -228,6 +228,72 @@ func apiSetToGVk(crds, apis APISet) (out []*api.GroupVersionKind) {
 			Version: a.Version,
 			Kind:    a.Kind,
 			Plural:  a.Plural,
+		})
+	}
+	return
+}
+
+func apiSetToDependencies(crds, apis APISet) (out []*api.Dependency) {
+	out = make([]*api.Dependency, 0)
+	for a := range crds {
+		val, err := json.Marshal(opregistry.GVKDependency{
+			Group:   a.Group,
+			Kind:    a.Version,
+			Version: a.Kind,
+		})
+		if err != nil {
+			panic(err)
+		}
+		out = append(out, &api.Dependency{
+			Type: opregistry.GVKType,
+			Value: string(val),
+		})
+	}
+	for a := range apis {
+		val, err := json.Marshal(opregistry.GVKDependency{
+			Group:   a.Group,
+			Kind:    a.Version,
+			Version: a.Kind,
+		})
+		if err != nil {
+			panic(err)
+		}
+		out = append(out, &api.Dependency{
+			Type: opregistry.GVKType,
+			Value: string(val),
+		})
+	}
+	return
+}
+
+func apiSetToProperties(crds, apis APISet) (out []*api.Property) {
+	out = make([]*api.Property, 0)
+	for a := range crds {
+		val, err := json.Marshal(opregistry.GVKProperty{
+			Group:   a.Group,
+			Kind:    a.Version,
+			Version: a.Kind,
+		})
+		if err != nil {
+			panic(err)
+		}
+		out = append(out, &api.Property{
+			Type: opregistry.GVKType,
+			Value: string(val),
+		})
+	}
+	for a := range apis {
+		val, err := json.Marshal(opregistry.GVKProperty{
+			Group:   a.Group,
+			Kind:    a.Version,
+			Version: a.Kind,
+		})
+		if err != nil {
+			panic(err)
+		}
+		out = append(out, &api.Property{
+			Type: opregistry.GVKType,
+			Value: string(val),
 		})
 	}
 	return
@@ -275,9 +341,11 @@ func bundle(name, pkg, channel, replaces string, providedCRDs, requiredCRDs, pro
 		CsvJson:      string(csvJson),
 		Object:       objs,
 		Version:      "0.0.0",
-		ProvidedApis: apiSetToGVk(providedCRDs, providedAPIServices),
-		RequiredApis: apiSetToGVk(requiredCRDs, requiredAPIServices),
+		ProvidedApis: apiSetToGVK(providedCRDs, providedAPIServices),
+		RequiredApis: apiSetToGVK(requiredCRDs, requiredAPIServices),
 		Replaces:     replaces,
+		Dependencies: apiSetToDependencies(requiredCRDs, requiredAPIServices),
+		Properties:   apiSetToProperties(providedCRDs, providedAPIServices),
 	}
 	for _, f := range opts {
 		f(b)
@@ -326,8 +394,8 @@ func bundleWithPermissions(name, pkg, channel, replaces string, providedCRDs, re
 		ChannelName:  channel,
 		CsvJson:      string(csvJson),
 		Object:       objs,
-		ProvidedApis: apiSetToGVk(providedCRDs, providedAPIServices),
-		RequiredApis: apiSetToGVk(requiredCRDs, requiredAPIServices),
+		ProvidedApis: apiSetToGVK(providedCRDs, providedAPIServices),
+		RequiredApis: apiSetToGVK(requiredCRDs, requiredAPIServices),
 	}
 }
 
