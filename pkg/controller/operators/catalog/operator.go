db02e7a9f5084c1a61f43697273aa882fee22f37
Evan <cordell.evan@gmail.com>
2020-07-15 18:12:34 -0400

    refactor dependency walking to avoid recursing

--- pkg/controller/operators/catalog/operator.go
+++ pkg/controller/operators/catalog/operator.go
@@ -130,7 +130,7 @@ func NewOperator(ctx context.Context, kubeconfigPath string, clock utilclock.Clo
 
 	var res resolver.Resolver
 	if resolverV2Enable {
-		res = resolver.NewSatStepResolver(lister, crClient, opClient.KubernetesInterface(), operatorNamespace)
+		res = resolver.NewSatStepResolver(lister, crClient, opClient.KubernetesInterface(), operatorNamespace, logger)
 	} else {
 		res = resolver.NewOperatorsV1alpha1Resolver(lister, crClient, opClient.KubernetesInterface(), operatorNamespace)
 	}
