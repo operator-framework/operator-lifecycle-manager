d6b1089b716ca8505c7884f159ae19957be763c9
Kyle Zhan <kylee.zh@gmail.com>
2020-10-23 09:41:51 +0800

    Specify cluster name when there is only one cluster in kind

--- scripts/build_local.sh
+++ scripts/build_local.sh
@@ -17,7 +17,15 @@ docker build -f e2e.Dockerfile -t quay.io/operator-framework/olm:local -t quay.i
 docker build -f test/e2e/hang.Dockerfile -t hang:10 ./bin
 
 if [ -n "$KIND" ]; then
-  kind load docker-image quay.io/operator-framework/olm:local
-  kind load docker-image quay.io/operator-framework/olm-e2e:local
-  kind load docker-image hang:10
+  CLUSTERS=($(kind get clusters))
+
+  # kind will use the cluster named kind by default, so if there is only one cluster, specify it
+  if [[ ${#CLUSTERS[@]} == 1 ]]; then
+    KIND_FLAGS="--name ${CLUSTERS[0]}"
+    echo 'Use cluster ${CLUSTERS[0]}'
+  fi
+
+  kind load docker-image quay.io/operator-framework/olm:local ${KIND_FLAGS}
+  kind load docker-image quay.io/operator-framework/olm-e2e:local ${KIND_FLAGS}
+  kind load docker-image hang:10 ${KIND_FLAGS}
 fi
