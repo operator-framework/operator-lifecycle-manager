a2f028d0ff0751674b21320f07213fd5bc41f4fa
Guillaume Rose <gurose@redhat.com>
2020-12-08 22:27:43 +0100

    Add default cluster profile annotation

--- scripts/copy_crds.sh
+++ scripts/copy_crds.sh
@@ -7,9 +7,14 @@ set -o pipefail
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 CRD_PATH="${SCRIPT_ROOT}/vendor/github.com/operator-framework/api/crds"
 
+rm "${SCRIPT_ROOT}"/deploy/chart/crds/*.yaml
 rm "${SCRIPT_ROOT}"/deploy/chart/templates/*.crd.yaml
 for f in "${CRD_PATH}"/*.yaml ; do
     echo "copying ${f}"
-    cp "${f}" "${SCRIPT_ROOT}/deploy/chart/templates/0000_50_olm_00-$(basename "$f" | sed 's/^.*_\([^.]\+\)\.yaml/\1.crd.yaml/')"
+    cp "${f}" "${SCRIPT_ROOT}/deploy/chart/crds/0000_50_olm_00-$(basename "$f" | sed 's/^.*_\([^.]\+\)\.yaml/\1.crd.yaml/')"
+
+    target="${SCRIPT_ROOT}/deploy/chart/templates/0000_50_olm_00-$(basename "$f" | sed 's/^.*_\([^.]\+\)\.yaml/\1.crd.yaml/')"
+    cp "${f}" "${target}"
+    sed -i '0,/annotations:/s//annotations:\n    {{- if and .Values.installType (eq .Values.installType "ocp") }}\n    include.release.openshift.io\/self-managed-high-availability: "true"\n    {{- end }}/' "${target}"
 done
 
