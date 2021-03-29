9e4cf2a0c2600e382ac068ac12310350e340cf62
Cesar Wong <cewong@redhat.com>
2021-03-29 15:50:45 -0400

    Add IBM managed cloud profile annotations to manifests
    Adds ibm-cloud-managed profile annotation to most manifests.
    For deployment manifests, creates an ibm-cloud-managed copy which
    removes the master node selector so that OLM can run on worker nodes.
    This should not impact other profiles or current function.
    
    Signed-off-by: Cesar Wong <cewong@redhat.com>

--- scripts/add_release_annotation.sh
+++ scripts/add_release_annotation.sh
@@ -8,6 +8,14 @@ chartdir=$1
 yq=$2
 
 for f in $chartdir/*.yaml; do
+   if [[ ! "$(basename "${f}")" =~ .*\.deployment\..* ]]; then
+      $yq w -d'*' --inplace --style=double $f 'metadata.annotations['include.release.openshift.io/ibm-cloud-managed']' true
+   else
+      g="${f/%.yaml/.ibm-cloud-managed.yaml}"
+      cp "${f}" "${g}"
+      $yq w -d'*' --inplace --style=double $g 'metadata.annotations['include.release.openshift.io/ibm-cloud-managed']' true
+      $yq d -d'*' --inplace $g 'spec.template.spec.nodeSelector."node-role.kubernetes.io/master"'
+   fi
    $yq w -d'*' --inplace --style=double $f 'metadata.annotations['include.release.openshift.io/self-managed-high-availability']' true
    $yq w -d'*' --inplace --style=double $f 'metadata.annotations['include.release.openshift.io/single-node-developer']' true
 done
