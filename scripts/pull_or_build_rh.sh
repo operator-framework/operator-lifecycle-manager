d918d71be9c8b752f3172b08e4c993823df95767
Evan <cordell.evan@gmail.com>
2021-07-20 08:42:48 -0400

    remove unused scripts
    kube_deps is not used for pinning kube dependencies anymore
    the other scripts were used for older ways that olm integrated with
    openshift and are no longer needed

--- scripts/pull_or_build_rh.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if ! docker pull "quay.io/coreos/olm:$1-rhel" || ! docker pull "quay.io/coreos/catalog:$1-rhel"; then
-  docker build -t "quay.io/coreos/olm:$1-rhel" -t "quay.io/coreos/catalog:$1-rhel" .
-  docker push "quay.io/coreos/olm:$1-rhel"
-  docker push "quay.io/coreos/catalog:$1-rhel"
-fi
