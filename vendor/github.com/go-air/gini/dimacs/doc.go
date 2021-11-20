88eeb7f1af414f1f78c6e792eb020825dfa775a8
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-21 06:07:43 +0800

    Delete vendor directory

--- vendor/github.com/go-air/gini/dimacs/doc.go
+++
@@ -1,5 +0,0 @@
-// Copyright 2016 The Gini Authors. All rights reserved.  Use of this source
-// code is governed by a license that can be found in the License file.
-
-// Package dimacs provides support for reading dimacs cnf files and variants.
-package dimacs
