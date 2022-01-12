b0382bb2ae1f14ef0ba8ca279ce91120208ccaff
timflannagan <timflannagan@gmail.com>
2022-01-11 23:31:48 -0500

    tools.go: Fix build constraints error message for the k8s.io/code-generator dependency
    Update the k8s.io/code-generator dependency and explicitly specify which
    packages are needed in order to update codegen/mockgen, and silence the
    following error locally:
    
    ```
    error while importing k8s.io/code-generator: build constraints exclude all Go files in ...
    ```
    
    Signed-off-by: timflannagan <timflannagan@gmail.com>

--- tools.go
+++ tools.go
@@ -11,7 +11,12 @@ import (
 	_ "github.com/onsi/ginkgo/ginkgo"
 	_ "github.com/operator-framework/api/crds" // operators.coreos.com CRD manifests
 	_ "helm.sh/helm/v3/cmd/helm"
-	_ "k8s.io/code-generator"
+	_ "k8s.io/code-generator/cmd/client-gen"
+	_ "k8s.io/code-generator/cmd/deepcopy-gen"
+	_ "k8s.io/code-generator/cmd/defaulter-gen"
+	_ "k8s.io/code-generator/cmd/informer-gen"
+	_ "k8s.io/code-generator/cmd/lister-gen"
+	_ "k8s.io/kube-openapi/cmd/openapi-gen"
 	_ "k8s.io/kube-openapi/cmd/openapi-gen"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 )
