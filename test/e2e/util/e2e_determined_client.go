22d5460bdfa731c6afef05ce41301cb61436bf38
perdasilva <perdasilva@redhat.com>
2022-06-14 21:40:18 +0200

    Refactor DeterminedE2eClient to DeterminedE2EClient
    Signed-off-by: perdasilva <perdasilva@redhat.com>

--- test/e2e/util/e2e_determined_client.go
+++ test/e2e/util/e2e_determined_client.go
@@ -8,19 +8,19 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-// DeterminedE2eClient wraps E2eClient calls in an Eventually assertion to keep trying
+// DeterminedE2EClient wraps E2eClient calls in an Eventually assertion to keep trying
 // or bail after some time if unsuccessful
-type DeterminedE2eClient struct {
+type DeterminedE2EClient struct {
 	*E2EKubeClient
 }
 
-func NewDeterminedClient(e2eKubeClient *E2EKubeClient) *DeterminedE2eClient {
-	return &DeterminedE2eClient{
+func NewDeterminedClient(e2eKubeClient *E2EKubeClient) *DeterminedE2EClient {
+	return &DeterminedE2EClient{
 		e2eKubeClient,
 	}
 }
 
-func (m *DeterminedE2eClient) Create(context context.Context, obj k8scontrollerclient.Object, options ...k8scontrollerclient.CreateOption) error {
+func (m *DeterminedE2EClient) Create(context context.Context, obj k8scontrollerclient.Object, options ...k8scontrollerclient.CreateOption) error {
 	m.keepTrying(func() error {
 		err := m.E2EKubeClient.Create(context, obj, options...)
 		return err
@@ -28,27 +28,27 @@ func (m *DeterminedE2eClient) Create(context context.Context, obj k8scontrollerc
 	return nil
 }
 
-func (m *DeterminedE2eClient) Update(context context.Context, obj k8scontrollerclient.Object, options ...k8scontrollerclient.UpdateOption) error {
+func (m *DeterminedE2EClient) Update(context context.Context, obj k8scontrollerclient.Object, options ...k8scontrollerclient.UpdateOption) error {
 	m.keepTrying(func() error {
 		return m.E2EKubeClient.Update(context, obj, options...)
 	})
 	return nil
 }
 
-func (m *DeterminedE2eClient) Delete(context context.Context, obj k8scontrollerclient.Object, options ...k8scontrollerclient.DeleteOption) error {
+func (m *DeterminedE2EClient) Delete(context context.Context, obj k8scontrollerclient.Object, options ...k8scontrollerclient.DeleteOption) error {
 	m.keepTrying(func() error {
 		return m.E2EKubeClient.Delete(context, obj, options...)
 	})
 	return nil
 }
 
-func (m *DeterminedE2eClient) Patch(context context.Context, obj k8scontrollerclient.Object, patch k8scontrollerclient.Patch, options ...k8scontrollerclient.PatchOption) error {
+func (m *DeterminedE2EClient) Patch(context context.Context, obj k8scontrollerclient.Object, patch k8scontrollerclient.Patch, options ...k8scontrollerclient.PatchOption) error {
 	m.keepTrying(func() error {
 		return m.E2EKubeClient.Patch(context, obj, patch, options...)
 	})
 	return nil
 }
 
-func (m *DeterminedE2eClient) keepTrying(fn func() error) {
+func (m *DeterminedE2EClient) keepTrying(fn func() error) {
 	Eventually(fn).Should(Succeed())
 }
