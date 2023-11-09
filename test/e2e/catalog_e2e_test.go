db0210e44e87a45e46ffedcee289f13ee8493ce3
Alexander Greene <greene.al1991@gmail.com>
2023-11-09 05:08:15 -0800

    Revert "Fix Image Update Test Cleanup"
    This reverts commit 2abfb3c18528b23f531db0b4baf64ab641f38036.
    
    Signed-off-by: Alexander Greene <greene.al1991@gmail.com>

--- test/e2e/catalog_e2e_test.go
+++ test/e2e/catalog_e2e_test.go
@@ -943,7 +943,7 @@ var _ = Describe("Starting CatalogSource e2e tests", func() {
 				APIGroup: "rbac.authorization.k8s.io",
 			},
 		}
-		roleBinding, err = c.CreateRoleBinding(roleBinding)
+		_, err = c.CreateRoleBinding(roleBinding)
 		Expect(err).ToNot(HaveOccurred(), "error granting registry-viewer permissions")
 		defer func() {
 			err := c.DeleteRoleBinding(roleBinding.GetNamespace(), roleBinding.GetName(), &metav1.DeleteOptions{})
