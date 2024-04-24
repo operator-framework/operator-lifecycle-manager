0697a4548774ef1a7a47606e9c91650062cbca69
Daniel Franz <dfranz@redhat.com>
2024-04-24 15:56:19 -0700

    Revert "Fix unpack job cache issue (#3204)"
    This reverts commit 47aaa6be4a951c6bd8be016f6610c5319adc6a47.

--- pkg/controller/bundle/bundle_unpacker.go
+++ pkg/controller/bundle/bundle_unpacker.go
@@ -671,9 +671,6 @@ func (c *ConfigMapUnpacker) ensureJob(cmRef *corev1.ObjectReference, bundlePath 
 	}
 	if len(jobs) == 0 {
 		job, err = c.client.BatchV1().Jobs(fresh.GetNamespace()).Create(context.TODO(), fresh, metav1.CreateOptions{})
-		if apierrors.IsAlreadyExists(err) {
-			job, err = c.client.BatchV1().Jobs(fresh.GetNamespace()).Update(context.TODO(), fresh, metav1.UpdateOptions{})
-		}
 		return
 	}
 
@@ -688,9 +685,6 @@ func (c *ConfigMapUnpacker) ensureJob(cmRef *corev1.ObjectReference, bundlePath 
 				if time.Now().After(cond.LastTransitionTime.Time.Add(unpackRetryInterval)) {
 					fresh.SetName(names.SimpleNameGenerator.GenerateName(fresh.GetName()))
 					job, err = c.client.BatchV1().Jobs(fresh.GetNamespace()).Create(context.TODO(), fresh, metav1.CreateOptions{})
-					if apierrors.IsAlreadyExists(err) {
-						job, err = c.client.BatchV1().Jobs(fresh.GetNamespace()).Update(context.TODO(), fresh, metav1.UpdateOptions{})
-					}
 				}
 			}
 
