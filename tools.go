f934a45a51fd0ccad8199e702b79e8942e6de908
Per Goncalves da Silva <pegoncal@redhat.com>
2024-05-07 14:18:58 +0200

    remove helm from tools.go

--- tools.go
+++ tools.go
@@ -7,7 +7,6 @@ import (
 	_ "github.com/golang/mock/mockgen"
 	_ "github.com/maxbrunsfeld/counterfeiter/v6"
 	_ "github.com/operator-framework/api/crds" // operators.coreos.com CRD manifests
-	_ "helm.sh/helm/v3/cmd/helm"
 	_ "k8s.io/code-generator"
 	_ "k8s.io/kube-openapi/cmd/openapi-gen"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
