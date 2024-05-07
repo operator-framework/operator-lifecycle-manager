e3078860691ad12747dac1538e2eb6e33f253cfa
Per Goncalves da Silva <pegoncal@redhat.com>
2024-05-07 14:13:11 +0200

    move yq to bingo
    Signed-off-by: Per Goncalves da Silva <pegoncal@redhat.com>

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -7,11 +7,15 @@ GO     ?= $(shell which go)
 
 # Below generated variables ensure that every time a tool under each variable is invoked, the correct version
 # will be used; reinstalling only if needed.
-# For example for counterfeiter variable:
+# For example for helm variable:
 #
 # In your main Makefile (for non array binaries):
 #
 #include .bingo/Variables.mk # Assuming -dir was set to .bingo .
+#
+#command: $(HELM)
+#	@echo "Running helm"
+#	@$(HELM) <flags/args..>
 #
 HELM := $(GOBIN)/helm-v3.14.4
 $(HELM): $(BINGO_DIR)/helm.mod
@@ -30,4 +34,10 @@ $(SETUP_ENVTEST): $(BINGO_DIR)/setup-envtest.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
 	@echo "(re)installing $(GOBIN)/setup-envtest-v0.0.0-20240507051437-479b723944e3"
 	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=setup-envtest.mod -o=$(GOBIN)/setup-envtest-v0.0.0-20240507051437-479b723944e3 "sigs.k8s.io/controller-runtime/tools/setup-envtest"
+
+YQ_V3 := $(GOBIN)/yq_v3-v3.0.0-20201202084205-8846255d1c37
+$(YQ_V3): $(BINGO_DIR)/yq_v3.mod
+	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
+	@echo "(re)installing $(GOBIN)/yq_v3-v3.0.0-20201202084205-8846255d1c37"
+	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=yq_v3.mod -o=$(GOBIN)/yq_v3-v3.0.0-20201202084205-8846255d1c37 "github.com/mikefarah/yq/v3"
 
