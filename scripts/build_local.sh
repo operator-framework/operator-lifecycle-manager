8c5352bc83c3b3540194733ed338a8166843e1ad
Per Goncalves da Silva <pegoncal@redhat.com>
2024-05-07 16:43:18 +0200

    remove cruft
    Signed-off-by: Per Goncalves da Silva <pegoncal@redhat.com>

--- scripts/build_local.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Note: run from root
-# This is used to start and build services for running e2e tests
-
-set -e
-set -o xtrace
-
-docker build -f e2e.Dockerfile -t quay.io/operator-framework/olm:local -t quay.io/operator-framework/olm-e2e:local ./bin
