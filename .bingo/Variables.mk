62b1b064bc9495887110a427ee5c8f7460e11bd9
Per Goncalves da Silva <pegoncal@redhat.com>
2024-05-07 10:09:31 +0200

    add seteup_envtest
    Signed-off-by: Per Goncalves da Silva <pegoncal@redhat.com>

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -7,15 +7,11 @@ GO     ?= $(shell which go)
 
 # Below generated variables ensure that every time a tool under each variable is invoked, the correct version
 # will be used; reinstalling only if needed.
-# For example for helm variable:
+# For example for counterfeiter variable:
 #
 # In your main Makefile (for non array binaries):
 #
 #include .bingo/Variables.mk # Assuming -dir was set to .bingo .
-#
-#command: $(HELM)
-#	@echo "Running helm"
-#	@$(HELM) <flags/args..>
 #
 HELM := $(GOBIN)/helm-v3.14.4
 $(HELM): $(BINGO_DIR)/helm.mod
@@ -23,9 +19,15 @@ $(HELM): $(BINGO_DIR)/helm.mod
 	@echo "(re)installing $(GOBIN)/helm-v3.14.4"
 	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=helm.mod -o=$(GOBIN)/helm-v3.14.4 "helm.sh/helm/v3/cmd/helm"
 
-KIND := $(GOBIN)/kind-v0.20.0
+KIND := $(GOBIN)/kind-v0.22.0
 $(KIND): $(BINGO_DIR)/kind.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/kind-v0.20.0"
-	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=kind.mod -o=$(GOBIN)/kind-v0.20.0 "sigs.k8s.io/kind"
+	@echo "(re)installing $(GOBIN)/kind-v0.22.0"
+	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=kind.mod -o=$(GOBIN)/kind-v0.22.0 "sigs.k8s.io/kind"
+
+SETUP_ENVTEST := $(GOBIN)/setup-envtest-v0.0.0-20240507051437-479b723944e3
+$(SETUP_ENVTEST): $(BINGO_DIR)/setup-envtest.mod
+	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
+	@echo "(re)installing $(GOBIN)/setup-envtest-v0.0.0-20240507051437-479b723944e3"
+	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=setup-envtest.mod -o=$(GOBIN)/setup-envtest-v0.0.0-20240507051437-479b723944e3 "sigs.k8s.io/controller-runtime/tools/setup-envtest"
 
