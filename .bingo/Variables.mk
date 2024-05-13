166e09d0c1243e85006c3e1cc8a8f556650a3c74
Per Goncalves da Silva <pegoncal@redhat.com>
2024-05-13 10:41:35 +0200

    fix bino/kind
    Signed-off-by: Per Goncalves da Silva <pegoncal@redhat.com>

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -23,9 +23,9 @@ $(HELM): $(BINGO_DIR)/helm.mod
 	@echo "(re)installing $(GOBIN)/helm-v3.14.4"
 	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=helm.mod -o=$(GOBIN)/helm-v3.14.4 "helm.sh/helm/v3/cmd/helm"
 
-KIND := $(GOBIN)/kind-v0.20.0
+KIND := $(GOBIN)/kind-v0.22.0
 $(KIND): $(BINGO_DIR)/kind.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/kind-v0.20.0"
-	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=kind.mod -o=$(GOBIN)/kind-v0.20.0 "sigs.k8s.io/kind"
+	@echo "(re)installing $(GOBIN)/kind-v0.22.0"
+	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=kind.mod -o=$(GOBIN)/kind-v0.22.0 "sigs.k8s.io/kind"
 
