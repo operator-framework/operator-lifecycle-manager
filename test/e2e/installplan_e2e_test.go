5ca24ea1ba2d5469cbe6e099f58dd5d907a63cdd
Per Goncalves da Silva <pegoncal@redhat.com>
2024-05-13 11:32:02 +0200

    minor fixes for installplan_e2e_test
    Signed-off-by: Per Goncalves da Silva <pegoncal@redhat.com>

--- test/e2e/installplan_e2e_test.go
+++ test/e2e/installplan_e2e_test.go
@@ -3968,7 +3968,7 @@ func newCRD(plural string) apiextensionsv1.CustomResourceDefinition {
 			Name: plural + ".cluster.com",
 		},
 		Spec: apiextensionsv1.CustomResourceDefinitionSpec{
-			Group: "cluster.com",
+			Group: genName("cluster") + ".com",
 			Versions: []apiextensionsv1.CustomResourceDefinitionVersion{
 				{
 					Name:    "v1alpha1",
