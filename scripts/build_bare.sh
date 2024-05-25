9639315fffec91dec8b174bcc843d6b1f6030408
Per Goncalves da Silva <pegoncal@redhat.com>
2024-05-25 16:43:24 +0200

    remove *_bare.sh scripts
    Signed-off-by: Per Goncalves da Silva <pegoncal@redhat.com>

--- scripts/build_bare.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Note: run from root
-# This is used to start and build services for running e2e tests
-
-set -e
-
-kubectl delete crds --all
-kubectl create namespace $(cat $(pwd)/e2e.namespace)
