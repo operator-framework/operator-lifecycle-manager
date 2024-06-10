a5a75f44efb58b29c081b21310a5f5ddf1558884
Per Goncalves da Silva <pegoncal@redhat.com>
2024-06-10 18:48:21 +0200

    ensure e2e fixture images are only pulled if not present
    Signed-off-by: Per Goncalves da Silva <pegoncal@redhat.com>

--- pkg/lib/image/image.go
+++ pkg/lib/image/image.go
@@ -13,5 +13,12 @@ func InferImagePullPolicy(image string) corev1.PullPolicy {
 	if strings.Contains(image, "@") {
 		return corev1.PullIfNotPresent
 	}
+
+	// Ensure test registry images are pulled only if needed
+	// These will normally be loaded through kind load docker-image
+	// This is also helps support the e2e fixture image re-build flow (see e2e GHA)
+	if strings.HasPrefix(image, "quay.io/olmtest/") {
+		return corev1.PullIfNotPresent
+	}
 	return corev1.PullAlways
 }
