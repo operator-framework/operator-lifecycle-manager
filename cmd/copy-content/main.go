65ef3b27f4bf091128f9e4df92dc60ca3120f2ff
Jordan Keister <jordan@nimblewidget.com>
2025-05-22 02:48:26 -0400

    make cache flags optional for extractContent functionality (#3586)
    Signed-off-by: grokspawn <jordan@nimblewidget.com>

--- cmd/copy-content/main.go
+++ cmd/copy-content/main.go
@@ -1,43 +1,55 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 
 	"github.com/otiai10/copy"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	catalogSource := flag.String("catalog.from", "", "Path to catalog contents to copy.")
-	catalogDestination := flag.String("catalog.to", "", "Path to where catalog contents should be copied.")
-	cacheSource := flag.String("cache.from", "", "Path to cache contents to copy.")
-	cacheDestination := flag.String("cache.to", "", "Path to where cache contents should be copied.")
-	flag.Parse()
+	cmd := newCmd()
+	cmd.Execute()
+}
 
-	for flagName, value := range map[string]*string{
-		"catalog.from": catalogSource,
-		"catalog.to":   catalogDestination,
-		"cache.from":   cacheSource,
-		"cache.to":     cacheDestination,
-	} {
-		if value == nil || *value == "" {
-			fmt.Printf("--%s is required", flagName)
-			os.Exit(1)
-		}
+func newCmd() *cobra.Command {
+	var (
+		catalogFrom string
+		catalogTo   string
+		cacheFrom   string
+		cacheTo     string
+	)
+	cmd := &cobra.Command{
+		Use:   "copy-content",
+		Short: "Copy catalog and cache content",
+		Long:  `Copy catalog and cache content`,
+		Run: func(cmd *cobra.Command, args []string) {
+			var contentMap = make(map[string]string, 2)
+			contentMap[catalogFrom] = catalogTo
+			if cmd.Flags().Changed("cache.from") {
+				contentMap[cacheFrom] = cacheTo
+			}
+
+			for from, to := range contentMap {
+				if err := os.RemoveAll(to); err != nil {
+					fmt.Printf("failed to remove %s: %s", to, err)
+					os.Exit(1)
+				}
+				if err := copy.Copy(from, to); err != nil {
+					fmt.Printf("failed to copy %s to %s: %s\n", from, to, err)
+					os.Exit(1)
+				}
+			}
+		},
 	}
 
-	for from, to := range map[string]string{
-		*catalogSource: *catalogDestination,
-		*cacheSource:   *cacheDestination,
-	} {
-		if err := os.RemoveAll(to); err != nil {
-			fmt.Printf("failed to remove %s: %s", to, err)
-			os.Exit(1)
-		}
-		if err := copy.Copy(from, to); err != nil {
-			fmt.Printf("failed to copy %s to %s: %s\n", from, to, err)
-			os.Exit(1)
-		}
-	}
+	cmd.Flags().StringVar(&catalogFrom, "catalog.from", "", "Path to catalog contents to copy")
+	cmd.Flags().StringVar(&catalogTo, "catalog.to", "", "Path to where catalog contents should be copied")
+	cmd.Flags().StringVar(&cacheFrom, "cache.from", "", "Path to cache contents to copy (required if cache.to is set)")              // optional
+	cmd.Flags().StringVar(&cacheTo, "cache.to", "", "Path to where cache contents should be copied (required if cache.from is set)") // optional
+	cmd.MarkFlagRequired("catalog.from")
+	cmd.MarkFlagRequired("catalog.to")
+	cmd.MarkFlagsRequiredTogether("cache.from", "cache.to")
+	return cmd
 }
