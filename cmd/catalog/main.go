c3368c547c439fed5d490ffe71fe7d03cf18cd73
Todd Short <todd.short@me.com>
2025-06-11 16:51:02 -0400

    Revert "Set resync to 6h"
    This reverts commit 855f940a2199bd4071c51f14ef44728550bf13cf.
    
    Signed-off-by: Todd Short <todd.short@me.com>

--- cmd/catalog/main.go
+++ cmd/catalog/main.go
@@ -27,7 +27,7 @@ import (
 
 const (
 	catalogNamespaceEnvVarName  = "GLOBAL_CATALOG_NAMESPACE"
-	defaultWakeupInterval       = 6 * time.Hour
+	defaultWakeupInterval       = 15 * time.Minute
 	defaultCatalogNamespace     = "olm"
 	defaultConfigMapServerImage = "quay.io/operator-framework/configmap-operator-registry:latest"
 	defaultOPMImage             = "quay.io/operator-framework/opm:latest"
